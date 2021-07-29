9e778feb501b050d22adea99387468135811c64e
Mitchellhogue0 <mitchell.hogue0@gmail.com>
2021-07-29 09:17:54 -0500

    commits lecture notes

--- selection_exercise.sql
+++ selection_exercise.sql
@@ -1,4 +1,15 @@
 use codeup_test_db;
 
+# animals whose type has 'dog' in it
 SELECT * FROM animals
 WHERE `type` LIKE '%dog%';
+
+# animals whose type of animal has an r in it
+SELECT * FROM animals
+WHERE name LIKE '%r%' LIMIT 1;
+
+# WHERE - IN
+SELECT name, type, age FROM animals WHERE size IN ('small', 'large');
+# IN is used for selecting multiple within one parameter (= is one, IN is more than one)
+
+

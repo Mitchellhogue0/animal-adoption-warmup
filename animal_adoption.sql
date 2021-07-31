68ded4669b5e55e553280f1c8cf41dd5485996eb
Mitchellhogue0 <mitchell.hogue0@gmail.com>
2021-07-30 19:01:44 -0500

    commits lecture notes

--- animal_adoption.sql
+++ animal_adoption.sql
@@ -22,3 +22,16 @@ SELECT name, CONCAT(name, ' was adopted on ', CURDATE()) FROM animals;
 
 
 # UNIX TIMESTAMP - number of seconds since jan first 1970
+
+# SELECT name FROM animals GROUP BY type;
+
+# SELECT all animals but display only the types of animals without repeating Group by
+SELECT type FROM animals GROUP BY type;
+SELECT * FROM animals;
+SELECT name, type FROM animals GROUP BY name, type;
+
+# AGGREGATE
+SELECT name, COUNT(name) as 'numberOfTimesNameAppears' FROM animals WHERE name NOT LIKE '%s%' GROUP BY name;
+
+# count - count the number of the column chosen
+SELECT age, AVG(age) FROM animals WHERE name NOT LIKE '%S%' GROUP BY age;
